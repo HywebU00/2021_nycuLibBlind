b6b82a27d63940d8284f74f4add4a14a3dddbe16
yachiung <38901575+yachiung@users.noreply.github.com>
2021-09-02 16:18:00 +0800

    第一次上傳

---
+++ js/calendar.js
@@ -0,0 +1,172 @@
+(function () {
+  window.onload = function () {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var today = now.getDate();
+    var weekday = now.getDay();
+    var $NOW = {
+      y: year,
+      m: month,
+      d: today };
+
+
+    var weekdays = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'];
+    var monthDays = [];
+    function setMonthDays(year) {
+      if (year % 4 == 0) {
+        // 闰年，二月有29天
+        monthDays = [31, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      } else {
+        // 平年
+        monthDays = [31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      }
+    }
+    setMonthDays(year);
+
+    now.setTime(now.setDate(1));
+    // 获取本月1号是星期几
+    var firstDayofThisMonth = now.getDay();
+    // 重置
+    now.setTime(now.setDate(today));
+
+    // 获取DOM
+    var monthDOM = document.getElementById('month');
+    var yearDOM = document.getElementById('year');
+    var weekdayDOM = document.getElementById('weekday');
+    var calandarBodyDOM = document.getElementById('calandar-body');
+    var nextDOM = document.getElementById('next-month');
+    var preDOM = document.getElementById('pre-month');
+    var goToTodayDOM = document.getElementById('go-to-today');
+
+    var calandarStart = function () {
+      monthDOM.innerHTML = month + '月';
+      yearDOM.innerHTML = year + '年';
+      weekdayDOM.innerHTML = weekdays[weekday];
+      // 创建日历主体
+      var currentMonthDay = 1;
+      var nextMonthDay = 1;
+      var preMonthDay = monthDays[month - 1];
+
+      var currentShowDays = monthDays[month] + firstDayofThisMonth;
+      var lines = Math.ceil(currentShowDays / 7);
+      for (let i = 0; i < lines; i++) {if (window.CP.shouldStopExecution(0)) break;
+        var row = document.createElement('div');
+        row.classList.add('row');
+        calandarBodyDOM.appendChild(row);
+        for (let j = 0; j < 7; j++) {if (window.CP.shouldStopExecution(1)) break;
+          var cell = document.createElement('div');
+          cell.classList.add('cell');
+          var pos = i * 7 + j + 1;
+          // 计算前一个月的日期
+          if (pos <= firstDayofThisMonth) {
+            cell.innerHTML = preMonthDay - (firstDayofThisMonth - pos);
+            cell.classList.add('cell-not-this-month');
+            cell.addEventListener('click', function (e) {
+              if (month == 1) {
+                year--;
+                month = 13;
+              }
+              goToDate(year, month - 1, +this.innerHTML);
+            }, false);
+          }
+          // 确定当月1号的位置, 并计算后续的日期
+          if (pos > firstDayofThisMonth) {
+            if (currentMonthDay <= monthDays[month]) {
+              if (currentMonthDay == today) {
+                cell.classList.add('today');
+              }
+              cell.innerHTML = currentMonthDay++;
+              cell.addEventListener('click', function (e) {
+                // var cells = calandarBodyDOM.getElementsByClassName('cell');
+                // cells = Array.prototype.slice.call(cells, 0);
+                // cells.forEach(function(item) {
+                //     if (item.classList.contains('today')) {
+                //         item.classList.remove('today');
+                //     }
+                // });
+                // this.classList.add('today');
+                goToDate(year, month, +this.innerHTML);
+              }, false);
+            } else {
+              // 计算下月的日期
+              cell.innerHTML = nextMonthDay++;
+              cell.classList.add('cell-not-this-month');
+              cell.addEventListener('click', function (e) {
+                if (month == 12) {
+                  year++;
+                  month = 0;
+                }
+                goToDate(year, month + 1, +this.innerHTML);
+              }, false);
+            }
+
+          }
+          row.appendChild(cell);
+        }window.CP.exitedLoop(1);
+      }window.CP.exitedLoop(0);
+    };
+    calandarStart();
+
+    // 跳转到指定日期，如跳转到 2016 01 01，则更新当前日历到2016年元旦
+    var goToDate = function (y, m, d) {
+      var date = new Date();
+      if (y >= 1970 && y <= 3000 && Math.floor(y) === y) {
+        date.setFullYear(y);
+        setMonthDays(y);
+      } else {
+        console.error('Calandar Not Support 1970- and 3000+');
+        return;
+      }
+      if (d >= 1 && d <= monthDays[m] && Math.floor(d) === d) {
+        date.setDate(d);
+      } else {
+        console.error('Wrong Day Format');
+        return;
+      }
+      if (m > 0 && m < 13 && Math.floor(m) === m) {
+        date.setMonth(m - 1);
+      } else {
+        console.error('Wrong Month Format');
+        return;
+      }
+
+      year = date.getFullYear();
+      month = date.getMonth() + 1;
+      today = date.getDate();
+      weekday = date.getDay();
+      date.setTime(date.setDate(1));
+      // 获取本月1号是星期几
+      firstDayofThisMonth = date.getDay();
+      // 重置
+      date.setTime(date.setDate(today));
+
+      // 清空calandar-body
+      var rows = calandarBodyDOM.getElementsByClassName('row');
+      rows = Array.prototype.slice.call(rows, 0);
+      rows.forEach(function (item) {
+        calandarBodyDOM.removeChild(item);
+      });
+      calandarStart();
+    };
+
+    // 月份跳转按钮
+    nextDOM.addEventListener('click', function (e) {
+      if (month == 12) {
+        year++;
+        month = 0;
+      }
+      goToDate(year, month + 1, 1);
+    }, false);
+    preDOM.addEventListener('click', function (e) {
+      if (month == 1) {
+        year--;
+        month = 13;
+      }
+      goToDate(year, month - 1, 1);
+    }, false);
+    goToTodayDOM.addEventListener('click', function (e) {
+      goToDate($NOW.y, $NOW.m, $NOW.d);
+    }, false);
+  };
+})();
---
+++ js/effect.js
@@ -0,0 +1,263 @@
+$(function(){
+
+  var _body = $('body, html');
+  var _window = $(window);
+
+  var ww = _window.width();
+  var wh = _window.height();
+  var wwNew = ww;
+
+  const wwSlim = 500;
+  const wwMedium = 700;
+  const wwNormal = 1020;
+  const wwWide = 1440;
+  const wwMaximum = 1920;
+
+  const speed = 400;
+  const speedFaster = 200;
+
+
+  // 主選單（桌機版）
+  var _menu = $('.webHeader .menu');
+  _menu.find('li').has('ul').addClass('hasChild');
+  var  _hasChildMenu = _menu.find('.hasChild');
+  _hasChildMenu.each(function(){
+    let _this = $(this);
+    let _a = _this.children('a');
+    let _subMenu = _this.children('ul');
+    _this.hover(
+      function(){
+        _subMenu.stop(true, false).slideDown(250);
+      },
+      function(){
+        _subMenu.stop(true, false).slideUp(150);
+      }
+    )
+
+    _a.keyup(function (e) { 
+      _subMenu.show();
+    });
+    _this.find('li:last>a').focusout(function (e) { 
+      _subMenu.hide();
+    });
+  })
+
+
+
+
+
+
+
+  // 側欄
+  var _sidebar = $('.sidebar');
+  _sidebar.before('<div class="sidebarCover"></div>');//製作遮罩
+  _menu.clone().prependTo(_sidebar);//複製主選單到側欄給行動版用
+  var _sidebarCover = _sidebar.prev('.sidebarCover');
+  var _sidebarCtrl = $('.sidebarCtrl');
+  var _sidebarMenu = _sidebar.find('.menu');
+
+  _sidebarCtrl.click(function(){
+    if (_sidebar.hasClass('reveal')) {
+      _sidebar.removeClass('reveal');
+      _sidebarCtrl.removeClass('closeIt');
+      _sidebarCover.fadeOut(speed);
+      _body.removeClass('noScroll');
+    } else {
+      _sidebar.addClass('reveal');
+      _sidebarCtrl.addClass('closeIt');
+      _sidebarCover.fadeIn(speed);
+      _body.addClass('noScroll')
+    }
+  })
+  _sidebarCover.click(function(){
+    _sidebar.removeClass('reveal');
+    _sidebarCover.fadeOut(speed);
+    _sidebarCtrl.removeClass('closeIt');
+    _body.removeClass('noScroll');
+  });
+
+  var _hasChild_sb = _sidebarMenu.find('.hasChild>a');
+  _hasChild_sb.click(
+    function(e){
+      e.preventDefault();
+
+      let _this = $(this);
+      let _subMenu = _this.next('ul');
+
+      if (_subMenu.is(':hidden')) {
+        _subMenu.stop(true, false).slideDown();
+        _this.parent().addClass('closeIt');
+      } else {
+        _subMenu.stop(true, false).slideUp();
+        _this.parent().removeClass('closeIt');
+      }
+    }
+  )
+
+
+
+  // 輔助偏好設定
+  var _assistantCtrl = $('.assistant>a');
+  var _astPreferences = $('.assistant').find('.preferences');
+  var _options = _astPreferences.find('.fontSize, .colorSet');
+  var _closePref = _astPreferences.find('.closeBtn>a');
+  // var _lastAndOut = _options.filter('.colorSet').find('li:last>a');
+  const hideSpeed = 250;
+
+  _options.each(function(){
+    let _this = $(this);
+    let _optionItems = _this.find('li>a');
+    _optionItems.click(function(){
+      $(this).parent().addClass('active').siblings().removeClass('active');
+    })
+  })
+
+  _assistantCtrl.click( function(){
+    if ( _astPreferences.is(':hidden') ) {
+      _astPreferences.stop(true, false).slideDown();
+    } else {
+      _astPreferences.stop(true, false).slideUp(hideSpeed);
+    }
+  });
+  _assistantCtrl.keyup( function(e){
+    if( e.which == 9 ){
+      if ( _astPreferences.is(':hidden') ) {
+        _astPreferences.stop(true, false).slideDown();
+      } else {
+        _astPreferences.stop(true, false).slideUp(hideSpeed);
+      }
+    }
+  });
+
+  _closePref.click( function(){ 
+    _astPreferences.stop(true, false).slideUp(hideSpeed);
+  });
+  _closePref.keyup(function(e){
+    if( e.which == 13 ){
+      _astPreferences.stop(true, false).slideUp(hideSpeed);
+    }
+  });
+  _closePref.focusout( function(e){
+    _astPreferences.stop(true, false).slideUp(hideSpeed);
+  });
+  
+
+
+
+
+
+
+
+
+
+
+  // 燈箱 --- 【會員登入】 
+  var _showLightbox = $('.showLightbox');
+  var _lightbox = $('.lightbox');
+
+  _lightbox.before('<div class="cover"></div>');
+
+  _showLightbox.click( function(){
+    let boxID = $(this).attr('data-id');
+    let _lightboxNow = _lightbox.filter( function(){ return $(this).attr('data-id') === boxID} );
+    _lightboxNow.add(_lightboxNow.prev('.cover')).stop(true, false).fadeIn(speed);
+    _lightboxNow.find('.closeThis>a').focus();
+    _body.addClass('noScroll');
+  });
+
+  _lightbox.each( function(){
+    let _this = $(this);
+    let boxID = _this.attr('data-id');
+    let _hideLightbox = _this.find('.closeThis>a');
+    let _cover = _this.prev('.cover');
+
+    _hideLightbox.add(_cover).on( 'click', closeLightbox);
+
+    function closeLightbox(){
+      _this.add(_cover).fadeOut(speed);
+      _body.removeClass('noScroll');
+      _showLightbox.filter( function(){ return $(this).attr('data-id') === boxID} ).find('a').focus();
+    }
+  })
+
+
+
+
+
+
+  //go top and bottom------------------------------------------
+	var _goTop = $('.goTop');
+  _goTop.click(function(e){
+    e.preventDefault();
+    _body.stop(true,false).animate({scrollTop: 0}, 600);
+  });
+
+	$(window).scroll(function() {
+		if ( $(this).scrollTop() > 200){
+			_goTop.addClass('show');
+		} else {
+      _goTop.removeClass('show');
+		}
+	});
+
+  // 條列頁 active 樣式
+  var _category = $('.category');
+  _category.each(function(){
+    let _item = $(this).find('li');
+    _item.click(function(){
+      $(this).addClass('active').siblings().removeClass('active');
+    })
+  })
+
+
+  // 開合區 slideToggle
+  var _slideToggle = $('.slideToggle');
+  _slideToggle.each(function(){
+    let _this = $(this);
+    let _ctrl = _this.find('.slideCtrl');
+    let _drawer = _this.find('.drawer');
+    let text1 = _ctrl.text();
+    let text2 = _ctrl.attr('data-altTitle');
+
+    if(_drawer.is(':hidden')) {
+      _ctrl.addClass('openIt').text(text2);
+    } else {
+      _ctrl.removeClass('openIt').text(text1);
+    }
+
+    _ctrl.click(function(){
+      if (_drawer.is(':visible')) {
+        _drawer.slideUp();
+        $(this).addClass('openIt').text(text2);
+      } else {
+        _drawer.slideDown();
+        $(this).removeClass('openIt').text(text1);
+      }
+    })
+  })
+
+
+
+
+
+  // 字體大小
+  var _fontSize = $('.fontSize');
+  var _sizeSelect = _fontSize.find('li');
+  var _fsArea = $('.contBox').add('.row>section[class]');
+  _sizeSelect.click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+    if ($(this).hasClass('large')) {
+      _fsArea.css('font-size', '1.4rem')
+    }
+    if ($(this).hasClass('medium')) {
+      _fsArea.css('font-size', '1.16rem')
+    }
+    if ($(this).hasClass('small')) {
+      _fsArea.css('font-size', '1rem')
+    }
+  })
+
+
+
+
+})
